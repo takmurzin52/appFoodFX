3a9de137903c8c471ef46b563817977845811375
takmurzin52 <matvejtakmurzin@gmail.com>
2024-11-10 21:12:02 +0500

    Пока работает. Перед этапом "заполним теперь комобобокс с выбором типа шоколада, правим метод initialize"

--- src/main/java/com/example/appfoodfx1/GUI/FoodFormController.java
+++ src/main/java/com/example/appfoodfx1/GUI/FoodFormController.java
@@ -1,5 +1,6 @@
 package com.example.appfoodfx1.GUI;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
@@ -27,9 +28,33 @@ public class FoodFormController  implements Initializable {
     public CheckBox chkWithPoppy;
     public CheckBox chkWithSesame;
 
+    final String FOOD_FRUIT = "Фрукт";
+    final String FOOD_CHOCOLATE = "Шоколадка";
+    final String FOOD_COOKIE = "Булочка";
+
+    // добавил новую функцию
+    public void updatePanes(String value) {
+        this.fruitPane.setVisible(value.equals(FOOD_FRUIT));
+        this.fruitPane.setManaged(value.equals(FOOD_FRUIT));
+        this.chocolatePane.setVisible(value.equals(FOOD_CHOCOLATE));
+        this.chocolatePane.setManaged(value.equals(FOOD_CHOCOLATE));
+        this.cookiePane.setVisible(value.equals(FOOD_COOKIE));
+        this.cookiePane.setManaged(value.equals(FOOD_COOKIE));
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        cmbFoodType.setItems(FXCollections.observableArrayList(
+                FOOD_FRUIT,
+                FOOD_CHOCOLATE,
+                FOOD_COOKIE
+        ));
 
+        cmbFoodType.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            // вызываю новую функцию
+            updatePanes((String) newValue);
+        });
+
+        // вызываю новую функцию при инициализации формы
+        updatePanes("");
     }
 }
-
